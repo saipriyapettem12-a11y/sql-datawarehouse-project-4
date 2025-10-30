663c719ef6b482735dd16e03320cdfea39ddca16
saipriyapettem12-a11y <saipriyasaipriya84@gmail.com>
2025-10-30 11:19:17 +0530

    Update insertingData.sql

--- scripts/silver/insertingData.sql
+++ scripts/silver/insertingData.sql
@@ -45,8 +45,24 @@ INSERT INTO silver.crm_cust_info (
 
 
 
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
 
-insert into silver.crm_prd_info (
+CREATE TABLE silver.crm_prd_info (
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+
+
+INSERT INTO silver.crm_prd_info (
     prd_id          ,
     cat_id         ,
     prd_key        ,
@@ -56,7 +72,7 @@ insert into silver.crm_prd_info (
     prd_start_dt    ,
     prd_end_dt       
 )
-select 
+SELECT 
 prd_id,
 Replace(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
 SUBSTRING(prd_key,7,len(prd_key)) as prd_key,
