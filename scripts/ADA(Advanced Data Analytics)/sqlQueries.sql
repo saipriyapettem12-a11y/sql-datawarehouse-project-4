3f464159582faf4f2ce900a892d8c9fbee7c8272
saipriyapettem12-a11y <saipriyasaipriya84@gmail.com>
2025-10-31 12:03:35 +0530

    Update sqlQueries.sql

--- scripts/ADA(Advanced Data Analytics)/sqlQueries.sql
+++ scripts/ADA(Advanced Data Analytics)/sqlQueries.sql
@@ -75,6 +75,32 @@ ORDER BY total_sales DESC;
 
 
 
+-- Data Segmentation
+
+-- From cost segment (Below 100, between 100-500, between 500-1000, above 1000) how many products are there?
+
+WITH product_segments AS (
+    SELECT
+        product_key,
+        product_name,
+        cost,
+        CASE 
+            WHEN cost < 100 THEN 'Below 100'
+            WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+            WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+            ELSE 'Above 1000'
+        END AS cost_range
+    FROM gold.dim_products
+)
+SELECT 
+    cost_range,
+    COUNT(product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC;
+
+
+
 
 -- Segmenting customers based on their lifespan and spending behaviour
 
